54dba08ddafede616634e8b83867af7d320e83f1
hujiyuta <hujiyuta@gmail.com>
2012-11-20 14:57:21 +0900

    Site updated at 2012-11-20 05:57:21 UTC

---
+++ blog/open.js
@@ -0,0 +1,9 @@
+var subwin;
+ function win_open() {
+   subwin = window.open	('profile.html','windowname','width=350,height=450');
+	<!-- 
+	//subwin = window.open('/source/blog/profile.html','Sample','width=600, height=300, scrollbars=yes, lacation=yes');
+	-->
+ }
+ 
+
---
+++ blog/profile.js
@@ -0,0 +1,3 @@
+function btn_onclick(){
+	window.alert('here is to show up my profile');
+}
